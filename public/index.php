97ed88bbb5b4fd44a049c24a7df729aafdfa2857
Makcim <mkirienkov120908@gmail.com>
2026-06-18 17:06:08 +0400

    CORS clean

--- public/index.php
+++ public/index.php
@@ -1,35 +1,25 @@
 <?php
 
+// ===== CORS =====
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: *');
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
     exit();
 }
+// ===== КОНЕЦ CORS =====
 
-if (!function_exists('request_parse_body')) {
-    function request_parse_body(): array {
-        return [$_POST, $_FILES];
-    }
-}
-
-use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
