3e2ea459ebb6376e35c17169a745012f42026f39
Makcim <mkirienkov120908@gmail.com>
2026-06-18 17:36:02 +0400

    Enable error reporting

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // ===== CORS =====
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: *');
