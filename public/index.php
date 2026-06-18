cb49cfcfbb050da63578fb24cd015ee623cc4802
Makcim <mkirienkov120908@gmail.com>
2026-06-18 16:14:36 +0400

    Add index.php with CORS

--- public/index.php
+++ public/index.php
@@ -1,5 +1,14 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 if (!function_exists('request_parse_body')) {
     function request_parse_body(): array {
         return [$_POST, $_FILES];
