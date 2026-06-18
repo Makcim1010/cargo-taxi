f521f38f17c892553feaa19ce5feadce7710a3fc
Makcim <mkirienkov120908@gmail.com>
2026-06-18 17:03:23 +0400

    Fix duplicate CORS headers

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'], // временно разрешаем все
+    //'allowed_origins' => [], // временно разрешаем все
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
